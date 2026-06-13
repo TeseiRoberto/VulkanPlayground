4ced65e5263c0353f9b30bc6691fa4f41f97095b
Tesei Roberto <roberto_tesei@yahoo.com>
2026-06-13 16:21:18 +0200

    Implemented retrival of graphic queue family index (Renderer::loadQueueFamilyIndices)

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -28,14 +28,34 @@ namespace vp {
                 inline bool     isInit() const          { return m_instance != VK_NULL_HANDLE; }
 
         private:
+
+                /**
+                 * @struct QueueFamilyIndices
+                 * Utility struct used to keep track of indices to Queue families
+                */
+                struct QueueFamilyIndices {
+                        uint32_t        graphicQueueIndex = UINT32_MAX;         ///< Index to the graphic queue family
+                };
+
+
                 bool            createInstance();
                 void            destroyInstance();
 
                 bool            pickPhysicalDevice();
 
+                bool            loadQueueFamilyIndices();
+                void            unloadQueueFamilyIndices();
+
+                bool            createLogicalDevice();
+                void            destroyLogicalDevice();
+
 
                 VkInstance              m_instance = VK_NULL_HANDLE;            ///< Vulkan instance used by the renderer
                 VkPhysicalDevice        m_physDevice = VK_NULL_HANDLE;          ///< Physical device used by the renderer
+                VkDevice                m_logicDevice = VK_NULL_HANDLE;         ///< Logical device used by the renderer
+
+                QueueFamilyIndices      m_queueFamilyIndices;                   ///< Indices to queue families required by the renderer
+                VkQueue                 m_gfxQueue = VK_NULL_HANDLE;            ///< Graphic queue to which the renderer submits commands
         };
 
 }
