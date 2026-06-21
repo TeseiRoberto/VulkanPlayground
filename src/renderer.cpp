af0ad274f851539ff0ebc03379943c443297c989
Tesei Roberto <roberto_tesei@yahoo.com>
2026-06-21 12:14:41 +0200

    Implemented creation of swapchain (Renderer::createSwapchain) with depth test capabilities.
    Implemented create/destroyImage methods.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -29,32 +29,17 @@ namespace vp {
         */
         bool Renderer::init(GLFWwindow* wnd)
         {
+                #define CHECK(x) if( !x ) { terminate(); return false; }
+
                 if( !createInstance() )
                         return false;
 
-                if( !pickPhysicalDevice() )
-                {
-                        terminate();
-                        return false;
-                }
-
-                if( !loadQueueFamilyIndices() )
-                {
-                        terminate();
-                        return false;
-                }
-
-                if( !createLogicalDevice() )
-                {
-                        terminate();
-                        return false;
-                }
-
-                if( !createSurface(wnd) )
-                {
-                        terminate();
-                        return false;
-                }
+                CHECK( pickPhysicalDevice() )
+                CHECK( loadQueueFamilyIndices() )
+                CHECK( createLogicalDevice() )
+                CHECK( createSurface(wnd) )
+                CHECK( createSwapchain(wnd) )
+                CHECK( createDepthAttachment() )
 
                 // TODO: Add other stuff...
                 
@@ -70,6 +55,8 @@ namespace vp {
         {
                 // TODO: Add other stuff...
 
+                destroyDepthAttachment();
+                destroySwapchain();
                 destroySurface();
                 destroyLogicalDevice();
                 unloadQueueFamilyIndices();
@@ -324,9 +311,11 @@ namespace vp {
 
         /**
          * @brief Renderer::createSurface
-         * Creates the surface on which the renderer will draw
+         * Creates the surface on which images produced by the renderer will be presented
          * @param wnd Window for which the surface shall be created
          * @return True on success, false on failure
+         * @note The surface is a vulkan abstraction for the platform specific window, and
+         * it is the target to which images to be displayed are sent
         */
         bool Renderer::createSurface(GLFWwindow* wnd)
         {
@@ -347,10 +336,8 @@ namespace vp {
 
 
         /**
-         * @brief Renderer::createSurface
-         * Creates the surface on which the renderer will draw
-         * @param wnd Window for which the surface shall be created
-         * @return True on success, false on failure
+         * @brief Renderer::destroySurface
+         * Destroys the surface on which images produced by the renderer are presented
         */
         void Renderer::destroySurface()
         {
@@ -361,6 +348,297 @@ namespace vp {
                 m_surface = VK_NULL_HANDLE;
         }
 
+
+        /**
+         * @brief Renderer::createSwapchain
+         * Creates the swapchain used by the renderer and acquires handles to its images
+         * @param wnd Window for which the swapchain shall be created
+         * @return True on success, false on failure
+         * @note The swapchain is simply an array of images, the renderer picks one of those images
+         * and draws on it, then sends it to the surface to be displayed; after that the renderer
+         * repeats the process but on another image of the array
+        */
+        bool Renderer::createSwapchain(GLFWwindow* wnd)
+        {
+                if(wnd == nullptr)
+                {
+                        LOG_ERROR("Renderer::init() failed: cannot create swapchain, given window is nullptr!");
+                        return false;
+                }
+
+                // Try to retrieve capabilities of the surface
+                VkSurfaceCapabilitiesKHR surfaceCaps {};
+                if( vkGetPhysicalDeviceSurfaceCapabilitiesKHR(m_physDevice, m_surface, &surfaceCaps) != VK_SUCCESS )
+                {
+                        LOG_ERROR("Renderer::init() failed: cannot create swapchain, vkGetPhysicalDeviceSurfaceCapabilitiesKHR() failed!");
+                        return false;
+                }
+
+                // Determine dimensions of swapchain images
+                m_swapchainProps.extent = surfaceCaps.currentExtent;
+                if(surfaceCaps.currentExtent.width = 0xFFFFFFFF)
+                {
+                        int width = 0;
+                        int height = 0;
+
+                        glfwGetFramebufferSize(wnd, &width, &height);
+
+                        m_swapchainProps.extent.width = static_cast<uint32_t>(width);
+                        m_swapchainProps.extent.height = static_cast<uint32_t>(height);
+                }
+
+                m_swapchainProps.imageFormat = VK_FORMAT_B8G8R8A8_SRGB;
+                const VkColorSpaceKHR imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+
+                VkSwapchainCreateInfoKHR swapchainInfo {};
+                swapchainInfo.sType             = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+                swapchainInfo.surface           = m_surface;
+                swapchainInfo.minImageCount     = surfaceCaps.minImageCount;
+                swapchainInfo.imageFormat       = m_swapchainProps.imageFormat;
+                swapchainInfo.imageColorSpace   = m_swapchainProps.colorSpace;
+                swapchainInfo.imageExtent       = m_swapchainProps.extent;
+                swapchainInfo.imageArrayLayers  = 1;
+                swapchainInfo.imageUsage        = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+                swapchainInfo.preTransform      = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+                swapchainInfo.compositeAlpha    = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+                swapchainInfo.presentMode       = VK_PRESENT_MODE_FIFO_KHR;
+
+                if( vkCreateSwapchainKHR(m_logicDevice, &swapchainInfo, nullptr, &m_swapchain) != VK_SUCCESS )
+                {
+                        LOG_ERROR("Renderer::init() failed: cannot create swapchain, vkCreateSwapchainKHR() failed!");
+                        return false;
+                }
+
+                // Aqquire handles to swapchain images
+                uint32_t imageCount = 0;
+                if( vkGetSwapchainImagesKHR(m_logicDevice, m_swapchain, &imageCount, nullptr) != VK_SUCCESS)
+                {
+                        LOG_ERROR("Renderer::init() failed: cannot acquire swapchain images, vkGetSwapchainImages() failed!");
+                        return false;
+                }
+
+                m_swapchainImages.resize(imageCount);
+                if( vkGetSwapchainImagesKHR(m_logicDevice, m_swapchain, &imageCount, m_swapchainImages.data()) != VK_SUCCESS)
+                {
+                        LOG_ERROR("Renderer::init() failed: cannot acquire swapchain images, vkGetSwapchainImages() failed!");
+                        return false;
+                }
+
+                return true;
+        }
+
+
+        /**
+         * @brief Renderer::destroySwapchain
+         * Destroys the swapchain used by the renderer
+        */
+        void Renderer::destroySwapchain()
+        {
+                if(m_swapchain == VK_NULL_HANDLE)
+                        return;
+
+                m_swapchainProps = {};
+                m_swapchainImages.clear();
+                vkDestroySwapchainKHR(m_logicDevice, m_swapchain, nullptr);
+
+                m_swapchain = VK_NULL_HANDLE;
+        }
+
+
+        /**
+         * @brief Renderer::createDepthAttachment
+         * Creates resources necessary for depth test
+         * @return True on success, false on failure
+        */
+        bool Renderer::createDepthAttachment()
+        {
+                // Depth image formats required by the renderer, from the most favourite to the last one
+                std::vector<VkFormat> candidates {
+                        VK_FORMAT_D32_SFLOAT_S8_UINT,
+                        VK_FORMAT_D24_UNORM_S8_UINT
+                };
+
+                VkFormat format = VK_FORMAT_UNDEFINED;
+
+                // Search a format that supports depth and stencil test too
+                for(VkFormat fmt : candidates)
+                {
+                        VkFormatProperties2 fmtProps {};
+                        fmtProps.sType = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2;
+
+                        vkGetPhysicalDeviceFormatProperties2(m_physDevice, fmt, &fmtProps);
+
+                        if(fmtProps.formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT)
+                        {
+                                format = fmt;
+                                break;
+                        }
+                }
+
+                if(format == VK_FORMAT_UNDEFINED)
+                {
+                        LOG_ERROR("renderer::init() failed: createDepthAttachment() failed, cannot find a valid format to create the depth attachment!");
+                        return false;
+                }
+
+                // Create image for the depth attachment
+                if( !createImage(m_depthAttachment, m_swapchainProps.extent.width, m_swapchainProps.extent.height,
+                                        VK_IMAGE_TYPE_2D, 1, format, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT) )
+                {
+                        LOG_ERROR("renderer::init() failed: createDepthAttachment() failed, creation of depth attachment image failed!");
+                        return false;
+                }
+
+                // Create view into the depth attachment image
+                VkImageViewCreateInfo imageViewInfo {};
+                imageViewInfo.sType     = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+                imageViewInfo.image     = m_depthAttachment.handle;
+                imageViewInfo.viewType  = VK_IMAGE_VIEW_TYPE_2D;
+                imageViewInfo.format    = format;
+
+                imageViewInfo.subresourceRange.aspectMask       = VK_IMAGE_ASPECT_DEPTH_BIT;
+                imageViewInfo.subresourceRange.levelCount       = 1;
+                imageViewInfo.subresourceRange.layerCount       = 1;
+        
+                if( vkCreateImageView(m_logicDevice, &imageViewInfo, nullptr, &m_depthAttachmentView) != VK_SUCCESS )
+                {
+                        LOG_ERROR("renderer::init() failed: createDepthAttachment() failed, creation of depth attachment image failed!");
+                        return false;
+                }
+
+                return true;
+        }
+
+
+        /**
+         * @brief Renderer::destroyDepthAttachment
+         * Destroyes resources used for depth test
+        */
+        void Renderer::destroyDepthAttachment()
+        {
+                // Destroy view into the depth attachment image
+                if(m_depthAttachmentView != VK_NULL_HANDLE)
+                {
+                        vkDestroyImageView(m_logicDevice, m_depthAttachmentView, nullptr);
+                        m_depthAttachmentView = VK_NULL_HANDLE;
+                }
+
+                destroyImage(m_depthAttachment);
+        }
+
+
+
+        /**
+         * @brief Renderer::createImage
+         * Creates a VkImage with the given parameteres and allocates memory for it
+         * @param image Output variable in which data for the created image will be stored
+         * @param width Width of the image to be crated, expressed in pixels
+         * @param height Height of the image to be crated, expressed in pixels
+         * @param type Type of image to be created
+         * @param mipLevels number of mip-map levels 
+         * @param format Format of the texels that will compose the image to be created
+         * @param tilingMode Specifies how the texels of the image should be arranged in memory
+         * @param usageFlags Flags describing how the image will be used
+         * @return True on success, false on failure
+         *
+        */
+        bool Renderer::createImage(Image& image, uint32_t width, uint32_t height, VkImageType type, uint32_t mipLevels,
+                        VkFormat format, VkImageTiling tilingMode, VkImageUsageFlags usageFlags)
+        {
+                VkImageCreateInfo imageInfo {};
+                imageInfo.sType                 = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+                imageInfo.imageType             = type;
+                imageInfo.format                = format;
+                imageInfo.extent.width          = width;
+                imageInfo.extent.height         = height;
+                imageInfo.mipLevels             = mipLevels;
+                imageInfo.arrayLayers           = 1;
+                imageInfo.samples               = VK_SAMPLE_COUNT_1_BIT;
+                imageInfo.tiling                = tilingMode;
+                imageInfo.usage                 = usageFlags;
+                imageInfo.initialLayout         = VK_IMAGE_LAYOUT_UNDEFINED;
+
+                // Create the vulkan image object
+                if( vkCreateImage(m_logicDevice, &imageInfo, nullptr, &image.handle) != VK_SUCCESS)
+                {
+                        LOG_ERROR("Renderer::createImage() failed: vkCreateImage() failed!");
+                        return false;
+                }
+
+                // Retrieve memory requirements for the image
+                VkMemoryRequirements imageMemRequirements;
+                vkGetImageMemoryRequirements(m_logicDevice, image.handle, &imageMemRequirements);
+        
+                VkMemoryAllocateInfo memAllocInfo {};
+                memAllocInfo.sType              = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+                memAllocInfo.allocationSize     = imageMemRequirements.size;
+                memAllocInfo.memoryTypeIndex    = findMemoryType(imageMemRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+        
+                // Allocate memory for the image
+                if( vkAllocateMemory(m_logicDevice, &memAllocInfo, nullptr, &image.memory) != VK_SUCCESS )
+                {
+                        LOG_ERROR("Renderer::createImage() failed: vkAllocateMemory() failed!");
+                        destroyImage(image);
+                        return false;
+                }
+
+                // Bind memory to the image
+                vkBindImageMemory(m_logicDevice, image.handle, image.memory, 0);
+
+                return true;
+        }
+
+
+        /**
+         * @brief Renderer::destroyImage
+         * Destroys the given image and deallocates its resources
+         * @param image Image to be destroyed
+        */
+        void Renderer::destroyImage(Image& image)
+        {
+                // Destroy the image object
+                if(image.handle != VK_NULL_HANDLE)
+                {
+                        vkDestroyImage(m_logicDevice, image.handle, nullptr);
+                        image.handle = VK_NULL_HANDLE;
+                }
+
+                // Free memory allocated for the image
+                if(image.memory != VK_NULL_HANDLE)
+                {
+                        vkFreeMemory(m_logicDevice, image.memory, nullptr);
+                        image.memory = VK_NULL_HANDLE;
+                }
+        }
+
+
+        /**
+         * @brief Renderer::findMemoryType
+         * Queries memory properties of the physical device used by the renderer and
+         * searches the right type of GPU memory to be used according to the given parameters
+         * @param typeFilter Bit field used to specify suitable memory types
+         * @param props Flags used to specify the properties that shall be satisfied by the memory type
+         * @return Index of the memory type (on the physical device) that shall be used,
+         *      UINT32_MAX if such memory type is not found
+        */
+        uint32_t Renderer::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags props)
+        {
+                VkPhysicalDeviceMemoryProperties memProps {};
+
+                // Query memory properties of the physical device
+                vkGetPhysicalDeviceMemoryProperties(m_physDevice, &memProps);
+
+                for(uint32_t i = 0; i < memProps.memoryTypeCount; ++i)
+                {
+                        if( typeFilter & (1 << i) && 
+                                (memProps.memoryTypes[i].propertyFlags & props) == props )
+                        {
+                                return i;
+                        }
+                }
+
+                return UINT32_MAX;
+        }
 
 }
 
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -38,6 +38,35 @@ namespace vp {
                 };
 
 
+                /**
+                 * @struct SwapchainProps
+                 * Utility struct used to keep track of properties of the
+                 * Swapchain used by the renderer
+                */
+                struct SwapchainProps {
+                        VkFormat                imageFormat;                    ///< Format used by the swapchain images
+                        VkColorSpaceKHR         colorSpace;                     ///< Color space used by the swapchain images
+                        VkExtent2D              extent = { 0, 0 };              ///< Size of the swapchain images
+                };
+
+
+                /**
+                 * @struct Image
+                 * Keeps track of the handle to a vulkan image object and the
+                 * allocated memory for the image data.
+                 *
+                 * @note The VkImage object defines the logical properties of a texture (texels format,
+                 *      texture size, mipmap levels), such object is used to specify how the GPU should
+                 *      interpret image data.
+                 *      The actual memory buffer in which image data gets stored must be allocated
+                 *      explicitly on the GPU, and then bound to the image object.
+                */
+                struct Image {
+                        VkImage                 handle = VK_NULL_HANDLE;        ///< Handle to the image object
+                        VkDeviceMemory          memory = VK_NULL_HANDLE;        ///< Handle to the memory buffer allocated for the image data
+                };
+
+
                 bool            createInstance();
                 void            destroyInstance();
 
@@ -52,15 +81,35 @@ namespace vp {
                 bool            createSurface(GLFWwindow* wnd);
                 void            destroySurface();
 
+                bool            createSwapchain(GLFWwindow* wnd);
+                void            destroySwapchain();
 
-                VkInstance              m_instance = VK_NULL_HANDLE;            ///< Vulkan instance used by the renderer
-                VkPhysicalDevice        m_physDevice = VK_NULL_HANDLE;          ///< Physical device used by the renderer
-                VkDevice                m_logicDevice = VK_NULL_HANDLE;         ///< Logical device used by the renderer
+                bool            createDepthAttachment();
+                void            destroyDepthAttachment();
 
-                QueueFamilyIndices      m_queueFamilyIndices;                   ///< Indices to queue families required by the renderer
-                VkQueue                 m_gfxQueue = VK_NULL_HANDLE;            ///< Graphic queue to which the renderer submits commands
+                bool            createImage(Image& image, uint32_t width, uint32_t height, VkImageType type, uint32_t mipLevels,
+                                                VkFormat format, VkImageTiling tilingMode, VkImageUsageFlags usageFlags);
+
+                void            destroyImage(Image& image);
+
+                uint32_t        findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags props);
+
+
+                VkInstance              m_instance = VK_NULL_HANDLE;                    ///< Vulkan instance used by the renderer
+                VkPhysicalDevice        m_physDevice = VK_NULL_HANDLE;                  ///< Physical device used by the renderer
+                VkDevice                m_logicDevice = VK_NULL_HANDLE;                 ///< Logical device used by the renderer
+
+                QueueFamilyIndices      m_queueFamilyIndices;                           ///< Indices to queue families required by the renderer
+                VkQueue                 m_gfxQueue = VK_NULL_HANDLE;                    ///< Graphic queue to which the renderer submits commands
         
-                VkSurfaceKHR            m_surface = VK_NULL_HANDLE;             ///< Surface on which the render will draw
+                VkSurfaceKHR            m_surface = VK_NULL_HANDLE;                     ///< Surface on which images produced by the renderer will be presented
+                
+                SwapchainProps          m_swapchainProps;                               ///< Properties of the swapchain used by the renderer
+                VkSwapchainKHR          m_swapchain = VK_NULL_HANDLE;                   ///< Swapchain that owns images on which renderer will draw
+                std::vector<VkImage>    m_swapchainImages;                              ///< Handles to images owned by the swapchain that the renderer is using
+        
+                Image                   m_depthAttachment;                              ///< Image used to implement depth test
+                VkImageView             m_depthAttachmentView = VK_NULL_HANDLE;         ///< View used for the deph attachment image
         };
 
 }
